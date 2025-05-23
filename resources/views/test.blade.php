65d0df8409bc051d783dee7e766ea0fa87a23807
0110vj <96834130+0110vj@users.noreply.github.com>
2025-05-23 14:39:20 +0800

    test

---
+++ resources/views/test.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.admin-layout')
+
+@section('title', 'Dietitian Dashboard')
+
+@section('content')
+<div class="container mt-4">
+    <h2>Welcome to the Dietitian Dashboard</h2>
+    <p>This is the main area for dietitian-related functionalities.</p>
+</div>
+@endsection
