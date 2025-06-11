ca7779acddaa6628a84a106372157c2d8479d1af
MikageReo <101623029+MikageReo@users.noreply.github.com>
2025-06-12 02:54:18 +0800

    Tambah use app models banking

--- app/Http/Controllers/ManageCartController.php
+++ app/Http/Controllers/ManageCartController.php
@@ -7,6 +7,7 @@ use App\Models\Cart;
 use App\Models\Order;
 use App\Models\OrderItem;
 use App\Models\User;
+use App\Models\Banking;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
